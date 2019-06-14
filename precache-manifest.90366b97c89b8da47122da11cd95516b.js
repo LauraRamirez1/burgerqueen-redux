f027357721699cfc23dd7b9655f09ba448502d8d
LauraRamirez1 <laura.ramirez1@gmail.com>
2019-06-14 15:13:31 -0400

    Updates

---
+++ precache-manifest.90366b97c89b8da47122da11cd95516b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16845272188824284dcb5727a265e120",
+    "url": "/burgerqueen-redux/index.html"
+  },
+  {
+    "revision": "b0f8f8bef6f136d82335",
+    "url": "/burgerqueen-redux/static/css/main.9ec4cf71.chunk.css"
+  },
+  {
+    "revision": "99d00dfd620c5ea62c9d",
+    "url": "/burgerqueen-redux/static/js/2.ff3dd737.chunk.js"
+  },
+  {
+    "revision": "b0f8f8bef6f136d82335",
+    "url": "/burgerqueen-redux/static/js/main.6421f185.chunk.js"
+  },
+  {
+    "revision": "fd5cd518f5af98e678fb",
+    "url": "/burgerqueen-redux/static/js/runtime~main.5a434e32.js"
+  },
+  {
+    "revision": "29029846471080b390c8d58943292a46",
+    "url": "/burgerqueen-redux/static/media/burguerQueen.29029846.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97b8710a78683b16a6fab649850b1874.js"
+  "/burgerqueen-redux/precache-manifest.90366b97c89b8da47122da11cd95516b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burgerqueen-redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
