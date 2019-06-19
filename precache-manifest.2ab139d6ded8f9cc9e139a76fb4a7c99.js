81e3106fbd37633b18c8b8626d155f3333ba65d8
LauraRamirez1 <laura.ramirez1@gmail.com>
2019-06-19 16:40:24 -0400

    Updates

---
+++ precache-manifest.2ab139d6ded8f9cc9e139a76fb4a7c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f12b4a2ddb5b69ac89b53b9719f107b",
+    "url": "/LauraRamirez1/burgerqueen-redux/index.html"
+  },
+  {
+    "revision": "251b408171ae52c6a538",
+    "url": "/LauraRamirez1/burgerqueen-redux/static/css/main.9ec4cf71.chunk.css"
+  },
+  {
+    "revision": "99d00dfd620c5ea62c9d",
+    "url": "/LauraRamirez1/burgerqueen-redux/static/js/2.ff3dd737.chunk.js"
+  },
+  {
+    "revision": "251b408171ae52c6a538",
+    "url": "/LauraRamirez1/burgerqueen-redux/static/js/main.2a7cbd41.chunk.js"
+  },
+  {
+    "revision": "86a4050e535fa4aa2166",
+    "url": "/LauraRamirez1/burgerqueen-redux/static/js/runtime~main.a855053a.js"
+  },
+  {
+    "revision": "29029846471080b390c8d58943292a46",
+    "url": "/LauraRamirez1/burgerqueen-redux/static/media/burguerQueen.29029846.png"
+  }
+]);
